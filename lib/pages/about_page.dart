31fd7064d4ce87db11cefe2b123dc0b3fc09c666
alexander <alex@gruessung.eu>
2021-05-26 07:30:46 +0200

    - ready for release 5.2.3
    Signed-off-by: Alexander <info@gruessung.eu>

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:oweapp4/services/AppLocalizations.dart';
+import 'package:oweapp4/services/Database.dart';
+import 'package:oweapp4/services/HaldeModel.dart';
 import 'package:oweapp4/widgets/drawer.dart';
 import 'package:package_info/package_info.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AboutPage extends StatefulWidget {
   static const String routeName = '/about';
@@ -13,9 +20,10 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   String versionName = '';
   String appName = '';
+  bool _allowWriteFile = false;
 
   _getBuildNumber() async {
-    if(appName.isEmpty) {
+    if (appName.isEmpty) {
       PackageInfo packageInfo = await PackageInfo.fromPlatform();
       setState(() {
         versionName = packageInfo.version;
@@ -34,7 +42,9 @@ class _AboutPageState extends State<AboutPage> {
     );
 
     return Scaffold(
-        appBar: AppBar(title: Text(AppLocalizations.of(context).translate('pagename_about'))),
+        appBar: AppBar(
+            title:
+                Text(AppLocalizations.of(context).translate('pagename_about'))),
         drawer: AppDrawer(),
         body: Center(
             child: Column(
@@ -49,7 +59,9 @@ class _AboutPageState extends State<AboutPage> {
                 height: 5,
               ),
               Text(
-                AppLocalizations.of(context).translate('app_name') + ' v' + this.versionName,
+                AppLocalizations.of(context).translate('app_name') +
+                    ' v' +
+                    this.versionName,
                 style: textStyle,
               ),
               Text("www.leihen.app", style: textStyle),
@@ -63,7 +75,89 @@ class _AboutPageState extends State<AboutPage> {
               Text("www.gruessung.eu", style: textStyle),
               Container(height: 50),
               Icon(Icons.favorite, color: Colors.redAccent),
-              Text(AppLocalizations.of(context).translate('widmung'), style: textStyle)
+              Text(AppLocalizations.of(context).translate('widmung'),
+                  style: textStyle),
+              Container(height: 70),
+              ElevatedButton(
+                  onPressed: () {
+                    _getStoragePersmission();
+                    final Future<List<Halde>> data =
+                        DBProvider.db.getAllItems();
+                    data.then((value) => {
+                         export(value)
+
+
+                        });
+
+
+
+                  },
+                  child: Text("Create Backup to debugging"))
             ])));
+  }
+
+  void export(value) {
+    String jsonString = "[";
+
+    value.forEach((Halde element) {
+      print(element.toMap());
+      jsonString = jsonString + element.toMap().toString();
+    });
+    jsonString = jsonString + "]";
+
+    _writeToFile(jsonString);
+
+    print(jsonString);
+  }
+
+  Future _writeToFile(String text) async {
+    if (!_allowWriteFile) {
+      return null;
+    }
+
+    final file = await _localFile;
+
+    // Write the file
+
+    File result = await file.writeAsString('$text');
+  }
+
+  Future get _localFile async {
+    final path = await _localPath;
+
+    return File('$path/lend-backup.json');
+  }
+
+  Future get _localPath async {
+    // Application documents directory: /data/user/0/{package_name}/{app_name}
+
+    final applicationDirectory = await getApplicationDocumentsDirectory();
+
+    // External storage directory: /storage/emulated/0
+
+    final externalDirectory = await getExternalStorageDirectory();
+
+    // Application temporary directory: /data/user/0/{package_name}/cache
+
+    final tempDirectory = await getTemporaryDirectory();
+
+    return externalDirectory.path;
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+
+  Future<PermissionStatus> _getStoragePersmission() async {
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.storage);
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.disabled) {
+      Map<PermissionGroup, PermissionStatus> permissionStatus =
+          await PermissionHandler()
+              .requestPermissions([PermissionGroup.storage]);
+      return permissionStatus[PermissionGroup.storage] ??
+          PermissionStatus.unknown;
+    } else {
+      return permission;
+    }
   }
 }
